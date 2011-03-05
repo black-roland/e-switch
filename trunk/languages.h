d1feed04a5422240825af30b4f733a5a8f0471b5
black_roland@mail.ru <black_roland@mail.ru@4f4ebc0e-bfc3-475f-02f6-5980ec39c720>
2011-03-05 02:49:11 +0000

    Версия 1.1 Исправлено восстановление последнего активного эльфа при перезапуске Добавлена возможность закрытия текущего эльфа при закрытии E-switch Добавлена возможность повторного запуска текущего эльфа
    Версия 1.1a
    Число действий увеличено до пяти
    Добавлено восстановление последнего активного действия при перезапуске
    Добавлена задержка перед выполнением нового действия при реконфиге
    
    Версия 1.0
    Первый релиз

---
+++ trunk/languages.h
@@ -0,0 +1,18 @@
+#ifdef LNG_RU
+  #include "languages\lng_ru.h"
+#endif
+#ifdef LNG_EN
+  #include "languages\lng_en.h"
+#endif
+#ifdef LNG_TR
+  #include "languages\lng_tr.h"
+#endif
+#ifdef LNG_UK
+  #include "languages\lng_uk.h"
+#endif
+#ifdef LNG_ES
+  #include "languages\lng_es.h"
+#endif
+#ifdef LNG_BG
+  #include "languages\lng_bg.h"
+#endif
---
+++ trunk/languages/lng_bg.h
@@ -0,0 +1,46 @@
+// Information for translators:
+// You can only replace text in quotes, for example
+// #define LNG_ELFS                  "<Text for replace>"
+
+// Информация для переводчиков:
+// Заменять можно только текст заключенный в кавычки, например
+// #define LNG_ELFS                  "<Этот текст можно изменить>"
+
+/* ---------- Settings/Настройки ---------- */
+#define LNG_ELFS                  "Elfs"
+#define LNG_ELF1                  "Elf 1"
+#define LNG_ELF2                  "Elf 2"
+#define LNG_ELF3                  "Elf 3"
+#define LNG_ELF4                  "Elf 4"
+#define LNG_ELF5                  "Elf 5"
+#define LNG_ACTION                "Действие"
+#define LNG_DISABLED              "Изключен"
+#define LNG_SWITCH_TO_ELF         "Превключване на Elf"
+#define LNG_BLANK                 "Празен"
+#define LNG_ELF_PATH              "Път към elf"
+#define LNG_BOOK_NAME             "Име на  book / elf"
+#define LNG_SETTINGS              "Настройки"
+#define LNG_SWITCH_KEY            "Промяна на ключ"
+#define LNG_ONLY_IN_SB            "Само в режим на готовност"
+#define LNG_VIBRATE               "Вибрация при включване"
+#define LNG_REDRAW_SB             "Изобразяване на StandByBook"
+#define LNG_CLS_A_W_C             "Затвори на елф, когато приключи"
+#define LNG_REPEAT                "Повторете стартирането на ключ"
+#define LNG_REPEAT_KEY            "Повторете стартирания ключ"
+/* ----------------------------------------- */
+
+/* ----------- Messages/Сообщения ---------- */
+#define LNG_ALREADY_RUNNED        "E-switch вече работи"
+#define LNG_INVALID_BOOK_NAME     "Неправилно име на книгата"
+#define LNG_ALL_ACTS_DISABLED     "Не са определени активни операции"
+#define LNG_BOOK_NOT_FOUND        "Elf за закриване не бе намерен. Проверете настройките"
+#define LNG_ELF_NOT_FOUND         "Elf за стартиране не е намерен. Проверете настройките"
+#define LNG_ELF_START_ERROR       "Грешка при пускане на елфите. Проверете настройките"
+/* ----------------------------------------- */
+
+/* -------------- Other/Прочее ------------- */
+#define LNG_NAME                  L"BG" // Language name/Краткое название языка
+//#define LNG_ABOUT_TRANSLATOR    L"\nTranslator: <Your name or nickname>"
+//#define LNG_ABOUT_TRANSLATOR    L"\nПереводчик: <Ваше имя или никнейм>"
+#define LNG_ABOUT_TRANSLATOR      L"\nПреводач: Добрин Тодоров"
+/* ----------------------------------------- */
